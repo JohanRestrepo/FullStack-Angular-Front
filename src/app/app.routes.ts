91d0f7466d157af592013c9762b7c27cf58374dd
Zarigueya <esnic1994@gmail.com>
2026-01-11 00:02:49 -0500

    he agregado el neuvo componente ejemplo y con las rutas me asegure que s emuestre su contenido y no el de la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Ejemplo } from './ejemplo/ejemplo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Ejemplo },
+];
