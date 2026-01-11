a1de06f53228e370131f9ca0577fafec0c6d50c7
Zarigueya <esnic1994@gmail.com>
2026-01-11 01:35:10 -0500

    He creado la clase customer y el servicio para customer

---
+++ src/app/customer.ts
@@ -0,0 +1,10 @@
+export class Customer {
+    constructor(
+        public id : number, 
+        public firstName: String, 
+        public lastName: String, 
+        public email: String
+    ){
+
+    }
+}
